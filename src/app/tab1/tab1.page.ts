2a09874ca0dd65d47e7123604f39c3162d6d3d50
Dev-OriolCC <cesaroriolcortezz@gmail.com>
2024-10-23 23:50:38 -0500

    Created Scan Tab and added static funtionality

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
+import { IonModalCustomEvent,IonToastCustomEvent,OverlayEventDetail } from '@ionic/core';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +8,46 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
+  
+  @ViewChild(IonModal) modal: IonModal | undefined;
+  
   constructor() {}
+  
+  message = 'This modal example uses triggers to automatically open a modal when the button is clicked.';
+  name: string | undefined;
+  
+  cancel() {
+    this.modal?.dismiss(null, 'cancel');
+  }
+  
+  confirm() {
+    this.modal?.dismiss(this.name, 'confirm');
+    // Loading
+    // Show result message and tags
+  }
+  
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirm') {
+      this.message = `Hello, ${ev.detail.data}!`;
+      console.log(ev.detail.data)
+    }
+  }
+
+  // Toast
+  public toastButtons = [
+    {
+      text: 'Dismiss',
+      role: 'cancel',
+      handler: () => {
+        console.log('Dismiss clicked');
+      },
+    },
+  ];
+  
+  setRoleMessage($event: IonToastCustomEvent<OverlayEventDetail<any>>) {
+    throw new Error('Method not implemented.');
+  }
+
 
 }
